807dbf9c3b67e114c8443dace317e561cdb6d30a
Your Name <you@example.com>
2025-09-07 17:55:01 +0530

    unused code removed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 
 import './App.css';
-import Weather from "./Weather";
+
 import WeatherApp from "./Weather";
 
 function App() {
   return (
     <div>
       <WeatherApp/>
-      {/*  <h3>Something Wrong</h3>*/}
     </div>
   );
 }
